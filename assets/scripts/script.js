ada26b53f8c04650fb7c32a50bd53c03a2adc570
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-09-28 17:36:01 +0930

    Completed F(gameStart)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -24,11 +24,16 @@ var quizQandA = [
 var wrapper = document.querySelector('#wrapper');
 
 
+
 //Vars for created elements needed across various functions
 var createDiv = document.createElement('div');
+var createDiv_game = document.createElement('div');
 var createH1 = document.createElement('h1');
 var createButton = document.createElement('button');
 var createUl = document.createElement('ul');
+var createWelcome = document.createElement('h1');
+var viewHiScores = document.createElement('button');
+var timerCountdown = document.createElement('h4');
 
 //Hoisted Vars
 var timer;
@@ -36,29 +41,48 @@ var timerCount = (quizQandA.length * 10); //15 seconds per quesiton
 
 
 function gameStart() {
-
+//Hides welcome txt and button
     createDiv.style.display = 'none';
-    console.log('working');
+                                                            console.log('working');
+//Creates new div for gameboard
+    createDiv_game.classList.add('gameDiv');
+    wrapper.appendChild(createDiv_game);
+    console.log('game div appened');
+// creates div within gameboard for header
+    var gameHeader = document.createElement('div');
+        gameHeader.classList.add('gameHeader');
+        createDiv_game.appendChild(gameHeader);
+                                                            console.log('gameheader div appened');
+// Adds view hi score button to game header
+        viewHiScores.classList.add('hiscores-btn');
+        viewHiScores.textContent = "View Hi Scores"
+        gameHeader.appendChild(viewHiScores);
 
-    createDiv.classList.add('gameDiv');
-    wrapper.appendChild(createDiv);
+// Adds timer element to header
+        timerCountdown.classList.add('timer-count');
+        timerCountdown.textContent = 'Time Left: ' + timerCount;
+        gameHeader.appendChild(timerCountdown);
 
+        
+    
 };
 
 function displaySpalsh() {
     //creates new wrapper for welcome text and start button
-    createDiv.classList.add('splashDiv');
-    wrapper.appendChild(createDiv);
+        createDiv.classList.add('splashDiv');
+        wrapper.appendChild(createDiv);
     //creates h1 to display game instructions
-    var createWelcome = document.createElement('h1');
         createWelcome.classList.add('splash-h1');
         createWelcome.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that incorrect answers will penalize your time/score by 15 seconds";
         createDiv.appendChild(createWelcome);
     var createButton = document.createElement('button');
-        createButton.classList.add('start-btn');
+        createButton.setAttribute('id', 'start-btn');
         createButton.textContent = 'start game';
         createDiv.appendChild(createButton);
-gameStart();
+
+    var gStart = document.querySelector("#start-btn");
+    gStart.addEventListener("click", gameStart);
+
 };
 
 
@@ -74,11 +98,11 @@ function renderQuestion() {
 function timer() {
     timer = setInterval(function() {
         timerCount--;
-        timerElement.textContent = "Time " + timerCount;
+        //timerElement.textContent = "Time " + timerCount;
         if (timerCount <=0) {
             clearInterval(timer);
             endGame()
-            timerElement.textContent = "Time's Up"
+            timerCountdown.textContent = "Time's Up"
         } 
     }, 1000);
 };
